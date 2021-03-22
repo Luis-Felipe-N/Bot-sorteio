d5df150196b0563cf6ff2c0142a602fbfbc8a8a4
Luis Felipe N <luisj2felipe09@gmail.com>
2021-03-21 22:36:06 -0300

    bot.altualização

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ import random
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 
 
-lista_de_contas = list()
-quant_vesez_comentado = 0
+
 index_conta = 0
+
+def trocarConta():
+    global index_conta
+
+    index_conta += 1
+
+    if index_conta == len(lista_de_contas):
+        print('Suas contas acabou, voltando para a primeira conta!')
+        index_conta = 0
+
+    login(lista_de_contas[index_conta][0], lista_de_contas[index_conta][1])
 
 
 def marcaAmigos(quant):
@@ -36,9 +45,9 @@ def marcaAmigos(quant):
 
 def login(email, senha):
     # Entrando no Instagram
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome(executable_path=r'C:chromedriver.exe')
     driver.get('https://www.instagram.com/')
-    sleep(19)
+    sleep(10)
 
     ## username
     campo_username = driver.find_element_by_xpath("//input[@name='username']")
@@ -52,7 +61,7 @@ def login(email, senha):
     campo_password.clear()
     campo_password.send_keys(senha)
     campo_password.send_keys(Keys.RETURN)
-    print(f'\033[32mUsando a conta {lista_de_contas[index_conta][0]}, com comentarios na intervalo de {tempo[0]} a {tempo[1]} segundos\033[m')
+    print(f'\033[32mUsando a conta {email}, com comentarios na intervalo de {tempo[0]} a {tempo[1]} segundos\033[m')
     sleep(10)
     comentarFoto(driver, foto)
     return driver
@@ -66,8 +75,7 @@ def digitar_como_uma_pessoa(frase, onde_digitar):
 
 # Comentar na foto
 def comentarFoto(driver, foto):
-    global index_conta
-    i = 0
+    quant_comentarios = 0
 
     # Entarando na foto
     driver.get(f'https://www.instagram.com/p/{foto}/')
@@ -75,46 +83,38 @@ def comentarFoto(driver, foto):
     print('\033[32mEntrou na foto com sucesso\033[m')
 
     # Comentar
-    while True:
-        try:
-            while True:
-                # lista de cometários
-                comentarios = marcaAmigos(quant_amigos_marca)
+    try:
+        while True:
+            # lista de cometários
+            comentarios = marcaAmigos(quant_amigos_marca)
 
-                # clicando no campo comtentário
-                driver.find_element_by_class_name('Ypffh').click()
-                campo_comentar = driver.find_element_by_class_name('Ypffh')
-                sleep(random.randint(1, 5))
+            # clicando no campo comtentário
+            driver.find_element_by_class_name('Ypffh').click()
+            campo_comentar = driver.find_element_by_class_name('Ypffh')
+            sleep(random.randint(1, 5))
 
-                # digitando um comentário aleatorio
-                digitar_como_uma_pessoa(comentarios, campo_comentar)
-                sleep(5)
-                campo_comentar.send_keys(Keys.RETURN)
-                sleep(random.randint(tempo[0],tempo[1]))
-                print('\033[32mComentando...\033[m')
+            # digitando um comentário aleatorio
+            digitar_como_uma_pessoa(comentarios, campo_comentar)
+            sleep(5)
+            campo_comentar.send_keys(Keys.RETURN)
+            sleep(random.randint(tempo[0],tempo[1]))
+            print('\033[32mComentando...\033[m')
 
-                # publicar
+            # publicar
 
-                i += 1
-                print(i)
-        except:
-            print(f'{i} comentarios')
-            print('\033[31mSua conta foi bloqueada\033[m \033[34m Trocando de conta\033[m')
+            quant_comentarios += 1
+            print(quant_comentarios)
+    except:
+        print(f'{quant_comentarios} comentarios')
+        print('\033[31mSua conta foi bloqueada\033[m \033[34m Trocando de conta\033[m')
 
-        # Fechar aba e aumentar o index da lista conta
-        index_conta += 1
         driver.quit()
-        sleep(2)
+        sleep(5)
 
-        # Se as conta acabar ele vai zerar o indedx e começar da primeira conta
-        if index_conta == len(lista_de_contas):
-            index_conta = 0
-            sleep(2)
+        trocarConta()
 
-        print(index_conta)
-        login(lista_de_contas[index_conta][0], lista_de_contas[index_conta][1])
-        
 
+lista_de_contas = list()
 # Colocando as  contas do insta num arquivo
 with open("lista_contas.txt", "r+") as contas:
         for conta in contas:
@@ -132,7 +132,7 @@ op = 1
 if op == 1:
     username = lista_de_contas[0][0]
     password = lista_de_contas[0][1]
-    tempo = [60, 70]
+    tempo = [5 , 2]
     foto = 'CMkHfHGFcZR'
     quant_amigos_marca = 2
 
